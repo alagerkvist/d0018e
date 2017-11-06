a22b306c87cdbb4cdb989e9d977ae6e266631e58
alagerkvist <lager.kvist@hotmail.com>
2017-11-06 16:34:54 +0100

    Login and registration works, needs more job with it tho

---
+++ include/conn.php
@@ -0,0 +1,10 @@
+<?php
+  try{
+    $conn = new PDO("mysql:host=utbweb.its.ltu.se;dbname=davjom5db", "davjom-5", "abc.123");
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    echo "IT WORKED!";
+  }catch(PDOException $e){
+    echo "Nononon";
+    $e->getMessage();
+  }
+?>
---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+  session_start();
+  include 'include/conn.php';
+  if(isset($_SESSION['user'])){
+    header("Location: index.php");
+    exit();
+  }
+
+  if(isset($_POST['sendRegister'])){
+    $stmt = $conn->prepare("INSERT INTO `user` (fName, lName, address, password, email) VALUES
+    (?, ?, ?, ?, ?)");
+
+    $stmt->bindParam(1, $_POST['fName']);
+    $stmt->bindParam(2, $_POST['lName']);
+    $stmt->bindParam(3, $_POST['address']);
+    $stmt->bindParam(4, $_POST['password']);
+    $stmt->bindParam(5, $_POST['email']);
+    $stmt->execute();
+  }
+
+?>
+
+<html>
+<head>
+  <title>Registrer</title>
+</head>
+<body>
+  <form method="post" action="register.php">
+  <p>First name: <input type="text" name="fName" /></p>
+  <p>Last name: <input type="text" name="lName" /></p>
+  <p>Address: <input type="text" name="address" /></p>
+  <p>Password: <input type="password" name="password" /></p>
+  <p>Email: <input type="text" name="email" /></p>
+  <p><input type="submit" name="sendRegister" value="Register!" /></p>
+  </form>
+</body>
+</html>
