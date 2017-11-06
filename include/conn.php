8bb89b1280fefcd9d7bbf9d1d418dc0c6c99e5d5
alagerkvist <lager.kvist@hotmail.com>
2017-11-06 16:37:12 +0100

    updated

--- include/conn.php
+++ include/conn.php
@@ -1,6 +1,6 @@
 <?php
   try{
-    $conn = new PDO("mysql:host=utbweb.its.ltu.se;dbname=davjom5db", "davjom-5", "abc.123");
+    $conn = new PDO("mysql:host=utbweb.its.ltu.se;dbname=davjom5db", "davjom-5", "***");
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     echo "IT WORKED!";
   }catch(PDOException $e){
