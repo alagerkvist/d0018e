1de258198d359aaf0701629486652a1374b50bbd
DEJohansson <33880373+DEJohansson@users.noreply.github.com>
2017-11-28 21:11:56 +0100

    Ignored and added a template for conn.php

---
+++ include/conn.template.php
@@ -0,0 +1,20 @@
+<?php
+  /*
+  * To connect to your database:
+  *   1. Copy file to the include folder, and rename to "conn.php"
+  *   2. Change the four variables to match your database credentials
+  */
+
+  $serverAddress = "localhost";
+  $dbName = "myDatabase"
+  $userName = "username";
+  $password = "password";
+
+  try{
+    $conn = new PDO("mysql:host=$serverAddress;dbname=$dbName", "$userName", "$password");
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    //echo "IT WORKED!";
+  }catch(PDOException $e){
+    $e->getMessage();
+  }
+?>
